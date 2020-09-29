63ff642af758e7739dd343815a0b439e8a31b55d
Lucas Miranda <lucas.miranda.silva00@gmail.com>
2020-09-29 16:50:40 -0300

    preço

---
+++ database/migrations/2020_09_29_191653_create_precos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrecosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('precos', function (Blueprint $table) {
+            $table->bigint('cdProduto');
+            $table->float('valor');
+            $table->float('desconto');
+            $table->boolean('flPromocao');
+            $table->dateTime('vigencia');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('precos');
+    }
+}
