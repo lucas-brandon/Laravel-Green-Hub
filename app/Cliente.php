238bd3b417946473f715ff943e372b667bc47c74
aluno <elly.estudos@gmail.com>
2020-09-29 12:33:47 -0300

    clientes

---
+++ app/Cliente.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+  public function index()
+  {
+
+    return 'sou um retorno no index Cliente';
+  }
+}
