e5645d6773d50a4542bd3d81d4a69af8f8cdebb3
Lucas Miranda <lucas.miranda.silva00@gmail.com>
2020-10-14 12:45:52 -0300

    api

--- app/Http/Controllers/Api/CartaoClienteController.php
+++ app/Http/Controllers/Api/CartaoClienteController.php
@@ -3,11 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\CartaoCliente;
+use App\Cliente;
 use http\Env\Response;
-
-
-
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Auth;
 use Illuminate\Http\Request;
 
 class CartaoClienteController extends BaseController
@@ -15,5 +14,17 @@ class CartaoClienteController extends BaseController
     public function __construct()
     {
         $this->classe = CartaoCliente::class;
+    }
+
+    public function cadastrar(Request $req)
+    {
+        //$dados = $req->all();
+        $dados['id_cliente'] = Cliente::user('id_cliente');
+        $dados['nr_cartao'] = $req->nr_cartao;
+        $dados['nome'] = Cliente::user('nome');
+        $dados['bandeira'] = $req->bandeira;
+        $dados['validade'] = $req->validade;
+
+        return response()->json(CartaoCliente::create($dados), 201);
     }
 }
--- app/Http/Controllers/Api/EnderecoClienteController.php
+++ app/Http/Controllers/Api/EnderecoClienteController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\EnderecoCliente;
+use App\Cliente;
+use App\Endereco;
 use http\Env\Response;
 
 
@@ -14,5 +16,14 @@ class EnderecoClienteController extends BaseController
     public function __construct()
     {
         $this->classe = EnderecoCliente::class;
+    }
+
+    public function cadastrar(Request $req)
+    {
+        $dados = $req->all();
+        $dados['id_cliente'] = Cliente::user('id_cliente');
+        $dados['id_endereco'] = Endereco::user('id_endereco');
+
+        return response()->json(EnderecoCliente::create($dados), 201);
     }
 }
