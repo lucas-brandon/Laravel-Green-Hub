68cc88c45cc5c60462a086b9e03d2ba8090cce4f
Lucas Brandon de Oliveira <luboliveira@RDL752683.raiadrogasil.com>
2020-11-11 17:38:05 -0300

    migração nova maquina

--- routes/web.php
+++ routes/web.php
@@ -96,3 +96,11 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 //Route::get('/home', 'Admin\HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
